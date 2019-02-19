97b25c1b5731d8ec3de15e548e2df2832786faae
Amit Thapa <amithapa1994@gmail.com>
2019-02-19 21:34:57 +0530

    Working

---
+++ 02_vars/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Using var
+	var name = "Tanishka"
+	var age int32 = 21
+	const isCool = true
+
+	isCool = false
+
+	fmt.Println(name, age, isCool)
+	fmt.Printf("%T\n", isCool)
+}
+
+// 18:45
+// https://www.youtube.com/watch?v=SqrbIlUwR0U&t=85s
